02f3c644b938952a9568e4103cd45dd328b3cbf8
Vinicius M Silva <vmsilva19@gmail.com>
2021-01-30 14:01:43 -0300

    Implemented trait try from | Fixed typo parse error property | Added method to get words from input

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,9 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -6,9 +11,54 @@ pub struct Request {
     method: Method,
 }
 
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        unimplemented!()
+    }
+}
+
 pub enum ParseError {
     InvalidRequest,
-    InvalidEnconding,
+    InvalidEncoding,
     InvalidProtocol,
     InvalidMethod,
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[i..], &request[..i]));
+        }
+    }
+
+    None
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
 }
